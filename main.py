cb77f38f36b6e11c39a10dd1547300173b00d72a
Rodrigo Costa <88461161+KaliRodri@users.noreply.github.com>
2021-08-26 03:21:59 +0000

    Major changes

--- main.py
+++ main.py
@@ -24,44 +24,105 @@ lista.append(p2)
 
 while (True):
  print("*******CENTRO DE DISTRIBUIÇÃO*******""\n")
- print("Digite a opção correspondente ao que deseja""\n""1 - Novo Cliente 2 - Ver Clientes 3 - Remover Cliente 4 - Sobre 5 - Mais opções 6 - Sair")
+ print("Digite a opção correspondente ao que deseja""\n""1 - Novo Cliente 2 - Ver Clientes 3 - Remover e Editar Cliente 4 - Sobre 5 - Mais opções 6 - Relatório 7 - Sair")
  option = int(input())
 
  if option == 1:
-  print("Qual o nome do seu novo Cliente?""\n")
-  Nome = input()
-
-  print("Qual o Cnpj do seu novo Cliente? Digite apenas os números, sem pontualção.""\n")
-  CNPJ = int(input())
+   print("Qual o nome do seu novo Cliente?""\n")
+   Nome = input()
   
-  print("Qual o Endereço do seu novo Cliente?""\n")
-  Endereco = input()
+   print("Qual o Cnpj do seu novo Cliente? Digite apenas os números, sem pontuação.""\n")
+   CNPJ = int(input())
+    
+   print("Qual o Endereço do seu novo Cliente?""\n")
+   Endereco = input()
   
-  print("Qual o produto que seu cliente compra?")
-  Produto = input()
+   print("Qual o produto que seu cliente compra?")
+   Produto = input()
+   23
   
-  lista.append(DadosCostumer(Nome, CNPJ, Endereco, Produto))
-  os.system("cls")
-  continue
+   lista.append(DadosCostumer(Nome, CNPJ, Endereco, Produto))
+   os.system("cls")
+   continue
 
  if option == 2:
   for i in lista:
-    i.printData()
-    time.sleep(1.0)
+    print("------------------------")
+    i.printName()
+  
+    print("Digite o nome do cliente para mais informações")
+    nome = input()
+    os.system("cls")
+    for i in lista:
+     if i.Nome == nome:
+      print(i.printData())
     continue
     
  if option == 3:
-   print("Qual o cpf do cliente a ser removido?")
-   Cnpj = str(input())
-   for i in lista:
-    if i.CNPJ == Cnpj:
-     lista.remove(i)
-     os.system("cls")
-     continue
+   print("Deseja editar ou remover um cliente?""\n" " 1 - Remover 2 - Editar")
+   choose = int(input())
+   
+   if choose == 1:
+    print("Qual o Cnpj do cliente a ser removido?")
+    Cnpj = str(input())
+    for i in lista:
+     if i.CNPJ == Cnpj:
+      lista.remove(i)
+      os.system("cls")
+    continue
+    
+   if choose == 2:
+     for i in lista:
+      print("Qual o Cnpj do cliente a ser editado?")
+      Cnpj = input()
+      
+      if i.CNPJ == Cnpj:
+       print("Oq vc quer editar: 1 Nome 2 Cnpj 3 Endereco 4 Produto")
+       choose = int(input())
+       if choose == 1:
+        print("Digite o novo nome: ")
+        newname = input()
+        i.Nome = newname
+        time.sleep(0.8)
+        i.printData()
+        input("Digite qq tecla para continuar:")
+        os.system("cls")
+        continue
+      
+       if choose == 2:
+        print("Digite o novo Cnpj: ")
+        newCnpj = input()
+        i.CNPJ = newCnpj
+        time.sleep(0.8)
+        i.printData()
+        input("Digite qq tecla para continuar:")
+        os.system("cls")
+        continue
+      
+       if choose == 3:
+        print("Digite o novo endereço: ")
+        newEdn = input()
+        i.Endereco = newEdn
+        time.sleep(0.8)
+        i.printData()
+        input("Digite qq tecla para continuar:")
+        os.system("cls")
+        
+      
+       if choose == 4:
+        print("Digite o novo produto: ")
+        newitem = input()
+        i.Produto = newitem
+        time.sleep(0.8)
+        i.printData()
+        input("Digite qq tecla para continuar:")
+        os.system("cls")
+   continue
   
+   
  if option == 4:
    print("Esse sistema foi desenvolvido em 25 de Agosto de 2021, foi criado e está sendo mantido por Ian Rodrigo, veja mais projetos meus em https://github.com/KaliRodri ")
-    
+   continue
  if option == 5:
    print("O que deseja ver? ""\n" "1 - Manual de Instruções 2 - FAQ")
    choose = int(input())
@@ -74,5 +135,13 @@ while (True):
      os.system("cls")
      continue
 
- if option < 5:
-    break
+ if option == 6:
+  for i in lista:
+   lines = ['relatorio', i.Nome, i.CNPJ, i.Endereco, i.Produto]
+   with open('relatorio.txt', 'w') as f:
+    for line in lines:
+     f.write(line)
+     f.write('\n')
+     
+ if option < 7:
+  break
